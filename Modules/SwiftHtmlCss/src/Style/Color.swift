b19351a2263deee8063d685289dba865a81510d1
Jason Zurita <jasonzurita@me.com>
2022-09-23 21:59:12 -0400

    feat: add new colors for header gradient

--- Modules/SwiftHtmlCss/src/Style/Color.swift
+++ Modules/SwiftHtmlCss/src/Style/Color.swift
@@ -1,3 +1,4 @@
+// TODO: consider a property that adds the `#`
 public struct Color {
     let hex: String
     public init(hex: String) {
--- Modules/SwiftyNotesSite/src/Colors.swift
+++ Modules/SwiftyNotesSite/src/Colors.swift
@@ -1,6 +1,9 @@
 import Foundation
 import SwiftHtmlCss
 
+// https://rgbacolorpicker.com/rgba-to-hex
 extension Color {
     static let gray = Color(hex: "F2F2F2")
+    static let headerTopBlue = Color(hex: "459bd0a8")
+    static let headerBottomBlue = Color(hex: "2F80ED")
 }
