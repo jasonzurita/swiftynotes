7a6bcb4825a92e335ad6b76d505b8ffaf5c71dfc
Jason Zurita <jasonzurita@me.com>
2022-10-04 07:33:40 -0400

    chore: add white color

--- Modules/SwiftyNotesSite/src/Colors.swift
+++ Modules/SwiftyNotesSite/src/Colors.swift
@@ -3,6 +3,7 @@ import SwiftHtmlCss
 
 // https://rgbacolorpicker.com/rgba-to-hex
 extension Color {
+    static let white = Color(hex: "FFFFFF")
     static let gray = Color(hex: "F2F2F2")
     static let headerTopBlue = Color(hex: "459bd0a8")
     static let headerBottomBlue = Color(hex: "2F80ED")
