e294b94d225bb8a1736ce5bf89749ddcdeba7c83
Jason Zurita <jasonzurita@me.com>
2022-10-04 22:35:09 -0400

    chore: sort the modifiers for more reliable tests
    If there are multiple modifiers, this will make sure the order
    is consistent to help with snapshot tests. Not necessary for
    the actual HTML, but to keep the tests from being flaky.

--- Modules/SwiftHtmlCss/src/HtmlNode.swift
+++ Modules/SwiftHtmlCss/src/HtmlNode.swift
@@ -11,12 +11,12 @@ public extension HtmlNode {
     var render: String {
         switch self {
         case let .element(el, attrs, copy, nested) where nested.isEmpty:
-            let attributes = attrs.isEmpty ? "" : " \(attrs.map { "\($0.0)=\"\($0.1)\"" }.joined(separator: " "))"
+            let attributes = attrs.isEmpty ? "" : " \(attrs.map { "\($0.0)=\"\($0.1)\"" }.sorted().joined(separator: " "))"
             return """
             <\(el)\(attributes)>\(copy)</\(el)>
             """
         case let .element(el, attrs: attrs, copy, nested):
-            let attributes = attrs.isEmpty ? "" : " \(attrs.map { "\($0.0)=\"\($0.1)\"" }.joined(separator: " "))"
+            let attributes = attrs.isEmpty ? "" : " \(attrs.map { "\($0.0)=\"\($0.1)\"" }.sorted().joined(separator: " "))"
             return """
             <\(el)\(attributes)>\(copy)
                  \(nested.map(\.render).joined(separator: "\n"))
