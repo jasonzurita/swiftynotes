757ea23ffe5e85a52218e9decf26659921a14738
Jason Zurita <jasonzurita@me.com>
2022-12-22 20:36:09 -0500

    feat(site): improve footer

--- Modules/SwiftyNotesSite/src/Colors.swift
+++ Modules/SwiftyNotesSite/src/Colors.swift
@@ -4,8 +4,9 @@ import SwiftHtmlCss
 // https://rgbacolorpicker.com/rgba-to-hex
 extension Color {
     static let white = Color(hex: "FFFFFF")
-    static let gray = Color(hex: "F2F2F2")
-    static let darkGray = Color(hex: "8a8a8a")
+    static let lightGray = Color(hex: "F2F2F2")
+    static let mediumGray = Color(hex: "8a8a8a")
+    static let darkGray = Color(hex: "333333")
     static let headerTopBlue = Color(hex: "459bd0a8")
     static let headerBottomBlue = Color(hex: "2F80ED")
 }
--- Modules/SwiftyNotesSite/src/Site/Site.swift
+++ Modules/SwiftyNotesSite/src/Site/Site.swift
@@ -8,14 +8,14 @@ func renderHtml() -> String {
                 SiteHeader()
                 P("Being built using an evolving Swift HTML result builder")
                 Footer {
-                    P("This site is ") {
+                    P("Jason Zurita &copy; 2022 | Built using an evolving Swift HTML result builder, which is") {
                         A(copy: "open source.", url: "https://github.com/jasonzurita/swiftynotes")
                     }
                 }
                 .textAlign(.center)
-                .color(.darkGray)
+                .color(.mediumGray)
             }
-            .background(.gray)
+            .background(.lightGray)
             .font(.apple)
             .textAlign(.center)
             .margin(0)
