1bbe3f81b734e9df851f835ad6c4cb0ea1a595f2
Jason Zurita <jasonzurita@me.com>
2022-12-26 20:40:32 -0500

    feat(site): create 'swifty notes' container (for the swift tips)
    This div will be the parent container for the swift tips

--- Modules/SwiftyNotesSite/src/Site/Site.swift
+++ Modules/SwiftyNotesSite/src/Site/Site.swift
@@ -6,7 +6,13 @@ func renderHtml() -> String {
             Head(title: "SwiftyNotes")
             Body {
                 SiteHeader()
-                P("Being built using an evolving Swift HTML result builder")
+                Div {
+                    P("Being built using an evolving Swift HTML result builder")
+                }
+                .margin([.top], 48)
+                .maxWidth(percent: 80)
+                .margin([.trailing, .leading], .auto)
+
                 Footer {
                     P("Jason Zurita &copy; 2022 | Built using an evolving Swift HTML result builder, which is") {
                         A(copy: "open source.", url: "https://github.com/jasonzurita/swiftynotes")
