ac5c562b498eed73aeb0dbf0e23e291bd80a0fc1
Jason Zurita <jasonzurita@me.com>
2023-02-22 15:04:04 -0500

    chore(site): update copyright year

--- Modules/SwiftyNotesSite/src/Site/RootSite.swift
+++ Modules/SwiftyNotesSite/src/Site/RootSite.swift
@@ -11,7 +11,7 @@ func renderHtml() -> String {
                 SiteHeader()
                 SiteNotes()
                 Footer {
-                    P("Jason Zurita &copy; 2022 | Built in Swift and ") {
+                    P("Jason Zurita &copy; 2023 | Built in Swift and ") {
                         A(copy: "open source.", url: "https://github.com/jasonzurita/swiftynotes")
                     }
                 }
