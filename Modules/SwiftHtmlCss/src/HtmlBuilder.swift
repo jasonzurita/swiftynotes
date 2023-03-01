eb60b8f0ca3ef0a5e46b06188c0323e8354d2d0e
Jason Zurita <jasonzurita@me.com>
2023-02-28 21:28:44 -0500

    feat(dsl): expand the capabilities of the html dsl
    With this change, the HTML eDSL now supports
    for loops, arrays, and if statements!
    
    This will be leveraged to simplify the creation of new
    notes in that they can now be stored in an array as
    metadata then looped over to list them for the site.
    Furthermore, this will unlock and simply having individual
    pages for each note to make sharing easier.

--- Modules/SwiftHtmlCss/src/HtmlBuilder.swift
+++ Modules/SwiftHtmlCss/src/HtmlBuilder.swift
@@ -1,6 +1,87 @@
 @resultBuilder
 public enum HtmlBuilder {
-    public static func buildBlock(_ components: HtmlProvider...) -> [HtmlNode] {
-        components.map(\.html)
+    // this is the highest level builder. All other builders like "buildArray" will
+    // be given the output of this since this will be applied to that subscope before
+    // the result is bubbled up to the higher level scope.
+    public static func buildBlock(_ components: [HtmlProvider]...) -> [HtmlNode] {
+        components.flatMap { $0 }.map(\.html)
+    }
+
+    // See note above. The output of this needs to match the input of that build block.
+    // There may be a limitation becuase of this where we can't have an array or
+    // for loop at the top level, but that should be an okay edge case.
+    //
+    // Also, `AnyElement` is used below because the type doesn't make much of a
+    // difference at this point.
+    public static func buildArray(_ components: [[HtmlNode]]) -> [HtmlProvider] {
+        return components.flatMap { $0 }.map {
+            switch $0 {
+            case let .element(element, attrs: attrs, copy, nodes):
+                return AnyElement(
+                    element: element,
+                    attrs: attrs,
+                    copy: copy,
+                    nodes: nodes
+                )
+            }
+        }
+    }
+
+    // This is used so that we promote all single HtmlProviders to the main currency
+    // for this result builders, which is an array of HtmlProviders. See the note
+    // in `buildBlock` above for its input.
+    public static func buildExpression(_ expression: HtmlProvider) -> [HtmlProvider] {
+        return [expression]
+    }
+
+    // This makes it so we can use an array literal
+    public static func buildExpression(_ expression: [HtmlProvider]) -> [HtmlProvider] {
+        return expression
+    }
+
+    // if/else statement support
+    public static func buildEither(first component: [HtmlNode]) -> [HtmlProvider] {
+        return component.map {
+            switch $0 {
+            case let .element(element, attrs: attrs, copy, nodes):
+                return AnyElement(
+                    element: element,
+                    attrs: attrs,
+                    copy: copy,
+                    nodes: nodes
+                )
+            }
+        }
+    }
+
+    // if/else statement support
+    public static func buildEither(second component: [HtmlNode]) -> [HtmlProvider] {
+        return component.map {
+            switch $0 {
+            case let .element(element, attrs: attrs, copy, nodes):
+                return AnyElement(
+                    element: element,
+                    attrs: attrs,
+                    copy: copy,
+                    nodes: nodes
+                )
+            }
+        }
+    }
+
+    // single if statement support
+    public static func buildOptional(_ component: [HtmlNode]?) -> [HtmlProvider] {
+        guard let c = component else { return [] }
+        return c.map {
+            switch $0 {
+            case let .element(element, attrs: attrs, copy, nodes):
+                return AnyElement(
+                    element: element,
+                    attrs: attrs,
+                    copy: copy,
+                    nodes: nodes
+                )
+            }
+        }
     }
 }
--- Modules/SwiftHtmlCss/src/HtmlElements/AnyElement.swift
+++ Modules/SwiftHtmlCss/src/HtmlElements/AnyElement.swift
@@ -3,7 +3,12 @@ import Foundation
 public struct AnyElement: HtmlProvider {
     public let html: HtmlNode
 
-    public init(element: String, attrs: [AttrType: String] = [:], copy: String = "", @HtmlBuilder content: () -> [HtmlNode]) {
+    public init(
+        element: String,
+        attrs: [AttrType: String] = [:],
+        copy: String = "",
+        @HtmlBuilder content: () -> [HtmlNode]
+    ) {
         html = .element(element, attrs: attrs, copy: copy, content())
     }
 
