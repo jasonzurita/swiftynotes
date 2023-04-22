6f6dd851efcde5f44c5b8ab2cb056947e5a6387d
Jason Zurita <jasonzurita@me.com>
2023-04-21 22:02:09 -0400

    feat(note): add new tip on biasing towards type inference

---
+++ Modules/SwiftyNotesSite/src/Notes/2023-04-21/avoid_declaring_types.snippet.swift
@@ -0,0 +1,9 @@
+// Leveraging Swift's type inference (Swifty-er!)
+let seven = 7 // type is an int
+let sevenPointZero = 7.0 // type is a double
+let luckyNumbers = [7, 7, 7] // type is an Array<Int>
+
+// Mostly bias away from doing this
+let five: Int = 5
+let fivePointZero: Double = 5.0
+let maybeLuckyNumbers: [Int] = [5, 5, 5]
