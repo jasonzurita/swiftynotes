0948cfb3e28a35a7f6c6f1717942bfe0cda06909
Jason Zurita <jasonzurita@me.com>
2023-05-26 09:45:26 -0400

    chore: minor syntax formatting

--- Modules/SwiftyNotesSite/src/Notes/2023-05-20/random_values.snippet.swift
+++ Modules/SwiftyNotesSite/src/Notes/2023-05-20/random_values.snippet.swift
@@ -1,5 +1,5 @@
-let numberLine = Double.random(in: 1...100)
+let numberLine = Double.random(in: 1 ... 100)
 let flipCoin = Bool.random()
-let rollDie = Int.random(in: 1...6)
+let rollDie = Int.random(in: 1 ... 6)
 let drinks = ["water", "cola", "apple juice", "orange juice"]
 let drinkRoulette = drinks.randomElement()
